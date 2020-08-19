14500c7a520c5dcc0ce594d01a66c6735989627e
Steven Cao <steven.cao@mychemist.com.au>
2020-08-19 12:42:48 +1000

    Add project files.

---
+++ src/MyProject.Admin/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using MyProject.Admin.Configuration;
+using MyProject.Admin.EntityFramework.Shared.DbContexts;
+using MyProject.Admin.EntityFramework.Shared.Entities.Identity;
+using MyProject.Admin.Helpers;
+using MyProject.Shared.Helpers;
+using System.Diagnostics;
+
+namespace MyProject.Admin
+{
+    public class Program
+    {
+        private const string SeedArgs = "/seed";
+
+        public static void Main(string[] args)
+        {
+            var configuration = GetConfiguration(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            Log.Information("Start Host");
+            try
+            {
+                DockerHelpers.ApplyDockerConfiguration(configuration);
+                var isService = !(Debugger.IsAttached || args.Contains("--console"));
+                if (isService)
+                {
+                    Log.Information("Start WS1");
+                    var host =CreateHostBuilder(args).UseWindowsService().Build();
+                    //await ApplyDbMigrationsWithDataSeedAsync(args, configuration, host);
+                    Log.Information("Start WS2");
+                    host.Run();
+                    Log.Information("Start WS3");
+                }
+                else
+                {
+                    Log.Information("Start Web");
+                    var host = CreateHostBuilder(args).Build();
+
+                    //await ApplyDbMigrationsWithDataSeedAsync(args, configuration, host);
+
+                    host.Run();
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static async Task ApplyDbMigrationsWithDataSeedAsync(string[] args, IConfiguration configuration, IHost host)
+        {
+            var applyDbMigrationWithDataSeedFromProgramArguments = args.Any(x => x == SeedArgs);
+            if (applyDbMigrationWithDataSeedFromProgramArguments) args = args.Except(new[] {SeedArgs}).ToArray();
+
+            var seedConfiguration = configuration.GetSection(nameof(SeedConfiguration)).Get<SeedConfiguration>();
+            var databaseMigrationsConfiguration = configuration.GetSection(nameof(DatabaseMigrationsConfiguration))
+                .Get<DatabaseMigrationsConfiguration>();
+
+            await DbMigrationHelpers
+                .ApplyDbMigrationsWithDataSeedAsync<IdentityServerConfigurationDbContext, AdminIdentityDbContext,
+                    IdentityServerPersistedGrantDbContext, AdminLogDbContext, AdminAuditLogDbContext,
+                    IdentityServerDataProtectionDbContext, UserIdentity, UserIdentityRole>(host,
+                    applyDbMigrationWithDataSeedFromProgramArguments, seedConfiguration, databaseMigrationsConfiguration);
+        }
+
+        private static IConfiguration GetConfiguration(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var isDevelopment = environment == Environments.Development;
+
+            var configurationBuilder = new ConfigurationBuilder()
+                //.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("serilog.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"serilog.{environment}.json", optional: true, reloadOnChange: true);
+
+            if (isDevelopment)
+            {
+                configurationBuilder.AddUserSecrets<Startup>();
+            }
+
+            configurationBuilder.AddCommandLine(args);
+            configurationBuilder.AddEnvironmentVariables();
+
+            return configurationBuilder.Build();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddJsonFile("hostsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+            return Host.CreateDefaultBuilder(args)
+                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 {
+                     configApp.AddJsonFile("serilog.json", optional: true, reloadOnChange: true);
+                     configApp.AddJsonFile("identitydata.json", optional: true, reloadOnChange: true);
+                     configApp.AddJsonFile("identityserverdata.json", optional: true, reloadOnChange: true);
+
+                     var env = hostContext.HostingEnvironment;
+
+                     configApp.AddJsonFile($"serilog.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                     configApp.AddJsonFile($"identitydata.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                     configApp.AddJsonFile($"identityserverdata.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                     if (env.IsDevelopment())
+                     {
+                         configApp.AddUserSecrets<Startup>();
+                     }
+
+                     configApp.AddEnvironmentVariables();
+                     configApp.AddCommandLine(args);
+                 })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options => options.AddServerHeader = false);
+                    webBuilder.UseStartup<Startup>().UseConfiguration(config); 
+                })
+                .UseSerilog((hostContext, loggerConfig) =>
+                {
+                    loggerConfig
+                        .ReadFrom.Configuration(hostContext.Configuration)
+                        .Enrich.WithProperty("ApplicationName", hostContext.HostingEnvironment.ApplicationName);
+                });
+        }
+    }
+}
+
+
+
+
+
